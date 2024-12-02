7e0cd3b7a9759c1d4c6724f6e4c9209bf0fe440b
josh-mdx <cv247@live.mdx.ac.uk>
2024-12-02 15:34:10 +0400

    added sort feature

--- public/js/index.js
+++ public/js/index.js
@@ -18,4 +18,25 @@ new Vue({
         searchQuery: '',
         searchstats: false
     },
+    mounted(){
+        this.fetchLessons();
+    },
+    methods:{
+        sortLessons(property, order) {
+             
+            this.lessons.sort((a, b) => { 
+                let aProp = property === 'price' ? Number(a[property]) : a[property];
+                let bProp= property === 'price' ? Number(b[property]) : b[property];
+                if (aProp > bProp) return order ? 1 : -1; 
+                if (aProp < bProp) return order ? -1 : 1; 
+                return 0; 
+            }); 
+        },
+        orderedLessons(){
+            console.log(this.sortBy);
+            this.sortLessons(this.sortBy, this.order);
+            this.order = !this.order;                     
+        },
+
+    }
 })
