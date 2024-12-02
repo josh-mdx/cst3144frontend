1cc80b9987758714dd5d14061f1ead714a2b1753
josh-mdx <cv247@live.mdx.ac.uk>
2024-12-02 15:35:37 +0400

    added both and remove from cart functionalities

--- public/js/index.js
+++ public/js/index.js
@@ -22,6 +22,99 @@ new Vue({
         this.fetchLessons();
     },
     methods:{
+        addToCart(lesson){
+            let les;
+            let cartLesson;
+            let newCartLesson;
+            let found;
+            // need to update both search and normal lesson arrays
+            if(this.searchstats){
+                for (let index = 0; index < this.searchedLessons.length; index++){
+                    les = this.searchedLessons[index]
+                    if(les.id === lesson.id){
+                        les.spaces--;
+                        les.taken++;
+                    }
+                }
+            }
+            for (let index = 0; index < this.lessons.length; index++) {
+                // using the id from the html to find the lesson in array
+                les = this.lessons[index]
+                if(les.id === lesson.id){
+                    les.spaces--;
+                    les.taken++;
+                    // add logic for empty cart array as well
+                    
+                    if(this.cart.length > 0){
+                        // checking if lesson is in cart 
+                        found = false;
+                        for (let i = 0; i < this.cart.length; i++){
+                            cartLesson = this.cart[i];
+                            if(lesson.id === cartLesson.id){
+                                found = true;
+                                break;
+                            }
+                        }
+                        if(found){
+                            cartLesson.amount++
+                        }
+                        else{
+                            newCartLesson = {id: les.id, name: les.name, price: les.price, imgurl: les.imgurl, amount: 1};
+                            this.cart.push(newCartLesson);
+                        }
+                        
+                    }
+                    else{
+                        newCartLesson = {id: les.id, name: les.name, price: les.price, imgurl: les.imgurl, amount: 1};
+                        this.cart.push(newCartLesson);
+                    }
+                    // console.log(this.cart)
+                    // this.cart.push(les);
+                    this.total++;
+                    break;
+                }
+                
+                
+            }
+        },
+        removeFromCart(lesson){
+            let les;
+            let cartLesson;
+            if(this.searchstats){
+                for (let index = 0; index < this.searchedLessons.length; index++){
+                    les = this.searchedLessons[index]
+                    if(les.id === lesson.id){
+                        les.spaces++;
+                        les.taken--;
+                    }
+                }
+            }
+            for (let index = 0; index < this.lessons.length; index++){
+                les = this.lessons[index]
+                if(les.id === lesson.id){
+                    les.spaces++;
+                    les.taken--;
+                    
+                    for (let i = 0; i < this.cart.length; i++){
+                        cartLesson = this.cart[i];
+                        if(lesson.id === cartLesson.id){
+                            
+                            if(cartLesson.amount > 1){
+                                cartLesson.amount--;
+                            }
+                            else{
+                                this.cart.splice(i, 1);
+                            }
+                            
+                        }                        
+
+                    }
+                    this.total--;
+                }
+                    
+            }
+            
+        },
         sortLessons(property, order) {
              
             this.lessons.sort((a, b) => { 
